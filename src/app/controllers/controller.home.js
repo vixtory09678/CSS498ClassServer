3c842478665e440680717283c68779e56460be35
roboticsza <robotics.za@gmail.com>
2020-04-20 03:22:16 +0700

    assignment1 web server

--- src/app/controllers/controller.home.js
+++ src/app/controllers/controller.home.js
@@ -9,19 +9,22 @@ exports.article = (req, res) => {
 exports.game = (req, res) => {
   console.log(req.query)
   res.locals.show = "" 
-  if(req.query.byte1.length == 0 || req.query.byte2.length == 0) {
-    res.locals.show = "fill in fields" 
+  if (req.query.byte1 == undefined){
+    
   }else {
-    if (req.query){
-      const byte1 = req.query.byte1
-      const byte2 = req.query.byte2
-      if (!isNaN(byte1) && !isNaN(byte2)) {
-        const number = parseInt(byte1) << 8 | parseInt(byte2)
-        if (number == 15000) {
-          res.locals.show = `your number <b>${number}</b> flag{29ff12a5e732465cef66878420f0fd9a}`
-        }else {
-          res.locals.show = `your number <b>${number}</b> unlucky`
-        }
+    if(req.query.byte1.length == 0 || req.query.byte2.length == 0) {
+      res.locals.show = "fill in fields" 
+      res.render('game')
+      return;
+    }
+    const byte1 = req.query.byte1
+    const byte2 = req.query.byte2
+    if (!isNaN(byte1) && !isNaN(byte2)) {
+      const number = parseInt(byte1) << 8 | parseInt(byte2)
+      if (number == 15000) {
+        res.locals.show = `your number <b>${number}</b> flag{29ff12a5e732465cef66878420f0fd9a}`
+      }else {
+        res.locals.show = `your number <b>${number}</b> unlucky`
       }
     }
   }
